054243990f4e2aaa36924e9b587407f918896a77
Jesus Hermosa <jesus.emmanuelhermosaa@avensia.com>
2023-05-21 03:13:52 +0800

    removed package

--- src/common/server/projectPhase.ts
+++ src/common/server/projectPhase.ts
@@ -12,31 +12,31 @@ export const getProjectPhase = async (): Promise<ProjectPhase> => {
     ],
   };
 
-  const response = await fetch(`${process.env.URL}/api/getExcelData`);
-  const excelData = await response.json();
+  // const response = await fetch(`${process.env.URL}/api/getExcelData`);
+  // const excelData = await response.json();
 
-  excelData.forEach((data: ExcelData, index: number) => {
-    const risk: Risk = {
-      category: data["CAT."],
-      area: data.AREA,
-      title: data.TITLE,
-      description: data.DESCRIPTION,
-      effectOrImpact: data["EFFECT / IMPACT"],
-      mitigation: data["MITIGATION (avoid risk)"],
-      contingency: data["CONTINGENCY PLAN (if risk occur)"],
-      impactProbability: data["IMPACTx PROBABILITY=SCORE | LEVEL"],
-      plannedAction: data["PLANNED ACTION"],
-      cellRow: index + 2,
-    };
+  // excelData.forEach((data: ExcelData, index: number) => {
+  //   const risk: Risk = {
+  //     category: data["CAT."],
+  //     area: data.AREA,
+  //     title: data.TITLE,
+  //     description: data.DESCRIPTION,
+  //     effectOrImpact: data["EFFECT / IMPACT"],
+  //     mitigation: data["MITIGATION (avoid risk)"],
+  //     contingency: data["CONTINGENCY PLAN (if risk occur)"],
+  //     impactProbability: data["IMPACTx PROBABILITY=SCORE | LEVEL"],
+  //     plannedAction: data["PLANNED ACTION"],
+  //     cellRow: index + 2,
+  //   };
 
-    projectPhase.categories.forEach((phase, index) => {
-      const { name } = phase;
-      if (!data[name]) return;
+  //   projectPhase.categories.forEach((phase, index) => {
+  //     const { name } = phase;
+  //     if (!data[name]) return;
 
-      let { risks } = projectPhase.categories[index];
-      projectPhase.categories[index].risks = [...risks, risk];
-    });
-  });
+  //     let { risks } = projectPhase.categories[index];
+  //     projectPhase.categories[index].risks = [...risks, risk];
+  //   });
+  // });
 
   return projectPhase;
 };
--- src/pages/api/auth/redirect.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (_req: NextApiRequest, res: NextApiResponse<any>) => {};
-
-export default handler;
