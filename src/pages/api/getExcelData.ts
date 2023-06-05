a1ef0d36ee1ed4e2ad9c38188aa139ced5b432c7
Jesus Hermosa <jesus.emmanuelhermosaa@avensia.com>
2023-06-06 03:50:04 +0800

    revalidation fix

--- src/pages/api/getExcelData.ts
+++ src/pages/api/getExcelData.ts
@@ -1,4 +1,4 @@
-import { readFile, utils } from "xlsx";
+import { read, readFile, utils } from "xlsx";
 import { ExcelData } from "@/common/types";
 import { NextApiRequest, NextApiResponse } from "next";
 import { excelFilePath } from "@/utils/getExcelFilePath";
@@ -9,14 +9,59 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
     return res.status(405).end();
   }
 
-  const workbook = await readFile(excelFilePath, {});
-  const sheet = workbook.Sheets[workbook.SheetNames[1]];
+  if (req.session.accessToken) {
+    const graphUrl = `https://graph.microsoft.com/v1.0/me/drives/${process.env.DRIVE_ID}/items/${process.env.DRIVE_ITEM_ID}`;
 
-  const data = utils.sheet_to_json<ExcelData>(sheet, {
-    defval: "",
-  });
+    const options = {
+      headers: {
+        Authorization: `Bearer ${req.session.accessToken}`,
+      },
+    };
 
-  res.status(200).json(data);
+    const response = await fetch(graphUrl, options);
+
+    if (!response.ok) {
+      console.error(response);
+      return res.status(500).send("graph fetch error");
+    }
+
+    const graphResponse = await response.json();
+    const workbookResponse = await fetch(
+      graphResponse["@microsoft.graph.downloadUrl"]
+    );
+
+    if (!workbookResponse.ok) return res.status(500).send("workbook error!");
+
+    try {
+      const resBlob = await workbookResponse.blob();
+      const resBufferArray = await resBlob.arrayBuffer();
+      const resBuffer = Buffer.from(resBufferArray);
+      const workbook = await read(resBuffer);
+      const sheet = workbook.Sheets[workbook.SheetNames[1]];
+
+      const data = utils.sheet_to_json<ExcelData>(sheet, {
+        defval: "",
+      });
+
+      return res.status(200).json(data);
+    } catch (error: any) {
+      console.error("revalidation error: ", error);
+      return res.status(500).send(error.message);
+    }
+  }
+
+  try {
+    const workbook = await readFile(excelFilePath, {});
+    const sheet = workbook.Sheets[workbook.SheetNames[1]];
+
+    const data = utils.sheet_to_json<ExcelData>(sheet, {
+      defval: "",
+    });
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(200).json(error);
+  }
 };
 
 export default handler;
--- src/pages/api/revalidateData.ts
+++ src/pages/api/revalidateData.ts
@@ -1,7 +1,5 @@
 import { withSessionRoute } from "@/common/server/sessionConfig";
 import { NextApiRequest, NextApiResponse } from "next";
-import { excelFilePath } from "@/utils/getExcelFilePath";
-import { read, writeFile } from "xlsx";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   if (req.method !== "GET") {
@@ -12,34 +10,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   if (!req.session.accessToken) {
     return res.redirect("/api/auth/signin");
   }
-  const graphUrl = `https://graph.microsoft.com/v1.0/me/drives/${process.env.DRIVE_ID}/items/${process.env.DRIVE_ITEM_ID}`;
-
-  const options = {
-    headers: {
-      Authorization: `Bearer ${req.session.accessToken}`,
-    },
-  };
-
-  const response = await fetch(graphUrl, options);
-
-  if (!response.ok) return res.status(500).send(response.text());
-
-  const graphResponse = await response.json();
-  const workbookResponse = await fetch(
-    graphResponse["@microsoft.graph.downloadUrl"]
-  );
-
-  if (!workbookResponse.ok) return res.status(500).send("workbook error!");
 
   try {
-    const resBlob = await workbookResponse.blob();
-    const resBufferArray = await resBlob.arrayBuffer();
-    const resBuffer = Buffer.from(resBufferArray);
-    const workbook = await read(resBuffer);
-
-    let newWorkBook = workbook;
-    await writeFile(newWorkBook, excelFilePath);
-
     await res.revalidate("/");
     return res.status(200).send("Success!");
   } catch (error: any) {
